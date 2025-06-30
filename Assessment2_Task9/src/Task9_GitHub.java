7a01a9cde578b43320ca01edb7bce7935619f4b5
Inerdhelp <suisos9014@gmail.com>
2025-06-30 13:20:15 +1200

    Initial upload of the project.

---
+++ Assessment2_Task9/src/Task9_GitHub.java
@@ -0,0 +1,17 @@
+/* BIT504
+* Michelle Bermudez 
+* Student ID: 5005549
+* Assessment 2
+*/
+
+
+
+public class Task9_GitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Course BIT504 - Programming 1");
+		System.out.println("For this project, I am writing a small amount of code.");
+		System.out.println("Thsi line will be removed later.");
+	}
+
+}
